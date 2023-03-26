02165bb62b59d00fedc00f77a2defa85e64f2e45
Zuzanna <zuzannaspychalska20@gmail.com>
2023-03-26 19:22:36 +0200

    Added new project with TextScript
    Nie dziala

---
+++ TextGameZuz/Assets/Scripts/TextGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextGame : MonoBehaviour
+{
+    //Program zgaduje liczbe ktora sobie wymyslilismy z przedzialu liczbowego
+    void Start()
+    {
+        Debug.Log("Witaj w grze");
+    }//print string
+
+    //cos
+    void Update()
+    {
+        
+    }
+}
